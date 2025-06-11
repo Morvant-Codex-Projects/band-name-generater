0460633805fff59bd54c948e26124e8c99d8606b
Danielle-Morvant <daniellemorvant1@gmail.com>
2025-06-11 12:08:02 -0500

    edited text in asyncAwait.js file

--- asyncAwait.js
+++ asyncAwait.js
@@ -8,7 +8,7 @@
 
 
 async function fetchAPI(){
-    await fetch("https://api.github.com/users").then(function(response){
+    await fetch("https://jsonplaceholder.typicode.com/users/1").then(function(response){
         return response.json()
     })
     .then(function(response){
